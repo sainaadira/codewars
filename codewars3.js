25cd5c34ce06c177018a9b5394f88a8c74f627b9
Sai <snaadira@gmail.com>
2019-07-01 17:12:39 -0400

    add codewars3

---
+++ codewars3.js
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+//
+//
+// Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//
+// For example,
+//
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+
+function countSheeps (arrayOfSheep) {
+    let sheepCounter = 0;
+    for( let i = 0; i < arrayOfSheep.length; i++) {
+      if(arrayOfSheep[i]=== true) {
+        sheepCounter++;
+      }
+    }
+    return sheepCounter;
+  }
+
+
+// My Test
+let sheep=[true, false, true, true, false, true, true
+false, false, true, true, true, true, false, false, true]
+
+
+
+// *******************************************************************
