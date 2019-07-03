4990fdad0f51ab8e4fc6f066aca98d8a4401b9f5
Sai <51007657+sainaadira@users.noreply.github.com>
2019-07-02 22:06:09 -0400

    Add files via upload

--- codewars3.js
+++ codewars3.js
@@ -30,3 +30,12 @@ false, false, true, true, true, true, false, false, true]
 
 
 // *******************************************************************
+
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+//
+function removeExclamationMarks(s) {
+  return s.split('!').join('');
+}
+
+console.log ("Hello World!")
