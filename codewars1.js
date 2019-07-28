852f2894077dc81fa33fefb7b96d4075627de9a0
Sai <snaadira@gmail.com>
2019-07-28 16:42:41 -0400

    updated codewars

--- codewars1.js
+++ codewars1.js
@@ -10,8 +10,9 @@
 function positiveSum(arr) {
  let sum = 0;
  for(let i = 0; i < arr.length; i++) {
+   // need a loop to go through the array
    if(arr[i] > 0) {
-    sum += arr[i];
+    sum += arr[i]
   }
  }
 return sum;
